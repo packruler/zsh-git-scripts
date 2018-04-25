a65559cfbcc3ae710e96ee5d53526361fedd53f3
Ethan Leisinger <eleisinger@gmail.com>
2018-04-25 12:21:24 -0400

    Add git-remove-merged

--- zsh-git-scripts.plugin.zsh
+++ zsh-git-scripts.plugin.zsh
@@ -34,3 +34,85 @@ function git-squash-branch() {
     git reset --soft HEAD~$count_commits
     git commit
 }
+
+function git-remove-merged() {
+    echo "What branch would you like to use as base: (Default: master)"
+    read  BASE_BRANCH
+
+    BASE_BRANCH=${BASE_BRANCH:-master}
+    EXCLUDE_STRING="master"
+    if [ "$BASE_BRANCH" != "master" ]; then
+        # Add the base branch to exclude pattern with master
+        EXCLUDE_STRING="master\|$BASE_BRANCH"
+    fi
+
+    # Build the command to get the names of branches that have been merged into the base, excluding those in the EXCLUDE_STRING
+    COMMAND="git branch --merged $BASE_BRANCH | grep -v '^[ *]*$EXCLUDE_STRING$'"
+
+    # Get branches from running COMMAND
+    BRANCHES_TO_DELETE=$(eval $COMMAND)
+    if [ ! "$BRANCHES_TO_DELETE" ]; then 
+        echo "No branches to delete"
+        return
+    fi
+
+    # Parse branches to array of values and print them for the user
+    BRANCHES_TO_DELETE=(${BRANCHES_TO_DELETE// / })
+    echo "Branches to be deleted:"
+    for i in "${!BRANCHES_TO_DELETE[@]}"
+    do
+        echo "${i}=>${BRANCHES_TO_DELETE[i]}"
+    done
+
+    if ! verify "Are you sure you would like to delete these? (y/n)" ; then
+        return
+    fi
+
+    # Convert array back to " " seperated string
+    BRANCHES_TO_DELETE=${BRANCHES_TO_DELETE[@]}
+
+    if verify "Would you like to delete both LOCAL and REMOTE versions, THIS CANNOT BE UNDONE? (y/n)" ; then
+        deleteLocal $BRANCHES_TO_DELETE
+        deleteRemote $BRANCHES_TO_DELETE
+        return
+    fi
+
+    if verify "Would you like to delete LOCAL versions? (y/n)" ; then
+        deleteLocal $BRANCHES_TO_DELETE
+    fi
+
+    if verify "Would you like to delete REMOTE versions? (y/n)" ; then
+        deleteRemote $BRANCHES_TO_DELETE
+    fi
+}
+
+# Function to verify user wants to do something accepting "y" or "n" as a response
+function verify()
+{
+    local PROMPT=${1:-"Are you sure? (y/n)"}
+    
+    while [[ true ]]; do
+        echo $PROMPT
+        read RESPONSE
+
+        if [ "$RESPONSE" == "y" ]; then
+            return 0
+        elif [ "$RESPONSE" == "n" ]; then
+            return 1
+        else
+            echo "Please enter valid response."
+        fi
+    done
+}
+
+function deleteLocal()
+{
+    local COMMAND="git branch -d $@"
+    eval $COMMAND
+}
+
+function deleteRemote()
+{
+    local COMMAND="git push origin -d $@"
+    eval $COMMAND
+}

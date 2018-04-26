9abab7e32741bd1078e72771364a1617f74ce6f6
Ethan Leisinger <packruler@gmail.com>
2018-04-26 09:19:48 -0400

    Update how git-remove-merged works as well as refactor how functions setup

--- git-remove-merged.zsh
+++ git-remove-merged.zsh
@@ -1,52 +1,76 @@
 #! /bin/zsh
 
+compdef _git-remove-merged git-remove-merged
+
+_git-remove-merged() {
+    _arguments \
+        ':branch-name:__branch_names'
+}
+
 function git-remove-merged() {
-    local BASE_BRANCH=${${1}:-master}
-    echo $BASE_BRANCH
-    return
-    EXCLUDE_STRING="master"
-    if [ "$BASE_BRANCH" != "master" ]; then
+    # Define functions that will only be used here locally
+    local function deleteLocal()
+    {
+        local COMMAND="git branch -d $@"
+        eval $COMMAND
+    }
+
+    local function deleteRemote()
+    {
+        local COMMAND="git push origin -d $@"
+        eval $COMMAND
+    }
+    
+    # Do actual work
+    local CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
+    
+    local EXCLUDE_STRING="master"
+    if [ "$CURRENT_BRANCH" != "master" ]; then
         # Add the base branch to exclude pattern with master
-        EXCLUDE_STRING="master\|$BASE_BRANCH"
+        EXCLUDE_STRING="master\|$CURRENT_BRANCH"
     fi
+    
+    
+    for i in $@
+    do
+        EXCLUDE_STRING="$EXCLUDE_STRING\|$i"
+    done
 
     # Build the command to get the names of branches that have been merged into the base, excluding those in the EXCLUDE_STRING
-    COMMAND="git branch --merged $BASE_BRANCH | grep -v '^[ *]*$EXCLUDE_STRING$'"
+    local COMMAND="git branch --merged $CURRENT_BRANCH | grep -v '^[ *]*$EXCLUDE_STRING$'"
 
     # Get branches from running COMMAND
-    BRANCHES_TO_DELETE=$(eval $COMMAND)
+    local BRANCHES_TO_DELETE=($(eval $COMMAND))
     if [ ! "$BRANCHES_TO_DELETE" ]; then 
         echo "No branches to delete"
         return
     fi
 
     # Parse branches to array of values and print them for the user
-    BRANCHES_TO_DELETE=(${BRANCHES_TO_DELETE// / })
     echo "Branches to be deleted:"
-    print -l $BRANCHES_TO_DELETE
-    # for i in "${!BRANCHES_TO_DELETE[@]}"
-    # do
-    #     echo "$i => $BRANCHES_TO_DELETE[i]"
-    # done
+    print -l ${BRANCHES_TO_DELETE[*]}
+    
+    verify "Are you sure you would like to delete these? (y/n)"
+    if (( $? == 1 )); then
+        return
+    fi
 
-    # if ! verify "Are you sure you would like to delete these? (y/n)" ; then
-    #     return
-    # fi
+    # Convert array back to " " seperated string
+    BRANCHES_TO_DELETE=${BRANCHES_TO_DELETE[*]}
+    verify "Would you like to delete both LOCAL and REMOTE versions, THIS CANNOT BE UNDONE? (y/n)"
+    if (( $? == 0 )) ; then
+        deleteLocal $BRANCHES_TO_DELETE
+        deleteRemote $BRANCHES_TO_DELETE
+        return
+    fi
 
-    # # Convert array back to " " seperated string
-    # BRANCHES_TO_DELETE=${BRANCHES_TO_DELETE[@]}
+    verify "Would you like to delete LOCAL versions? (y/n)"
+    if (( $? == 0 )) ; then
+        deleteLocal $BRANCHES_TO_DELETE
+    fi
 
-    # if verify "Would you like to delete both LOCAL and REMOTE versions, THIS CANNOT BE UNDONE? (y/n)" ; then
-    #     deleteLocal $BRANCHES_TO_DELETE
-    #     deleteRemote $BRANCHES_TO_DELETE
-    #     return
-    # fi
-
-    # if verify "Would you like to delete LOCAL versions? (y/n)" ; then
-    #     deleteLocal $BRANCHES_TO_DELETE
-    # fi
-
-    # if verify "Would you like to delete REMOTE versions? (y/n)" ; then
-    #     deleteRemote $BRANCHES_TO_DELETE
-    # fi
+    verify "Would you like to delete REMOTE versions? (y/n)"
+    if (( $? == 0 )) ; then
+        deleteRemote $BRANCHES_TO_DELETE
+    fi
 }
--- zsh-git-scripts.plugin.zsh
+++ zsh-git-scripts.plugin.zsh
@@ -1,19 +1,19 @@
 local SCRIPTPATH="$(cd "$(dirname "$0")" && pwd -P)"
 
 source $SCRIPTPATH/git-squash-branch.zsh
+source $SCRIPTPATH/git-remove-merged.zsh
 
 # Function to verify user wants to do something accepting "y" or "n" as a response
-function verify()
+verify()
 {
     local PROMPT=${1:-"Are you sure? (y/n)"}
     
     while  true ; do
         echo $PROMPT
         read RESPONSE
-
-        if [[ "$RESPONSE" -eq "y" ]]; then
+        if [ "$RESPONSE" = "y" ]; then
             return 0
-        elif [[ "$RESPONSE" -eq "n" ]]; then
+        elif [ "$RESPONSE" = "n" ]; then
             return 1
         else
             echo "Please enter valid response."
@@ -21,14 +21,20 @@ function verify()
     done
 }
 
-function deleteLocal()
-{
-    local COMMAND="git branch -d $@"
-    eval $COMMAND
+__command_successful () {
+  if (( ${#pipestatus:#0} > 0 )); then
+    _message 'not a git repository'
+    return 1
+  fi
+  return 0
 }
 
-function deleteRemote()
-{
-    local COMMAND="git push origin -d $@"
-    eval $COMMAND
+
+# Pulled function from https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/git-extras/git-extras.plugin.zsh
+__branch_names() {
+    local expl
+    declare -a branch_names
+    branch_names=(${${(f)"$(_call_program branchrefs git for-each-ref --format='"%(refname)"' refs/heads 2>/dev/null)"}#refs/heads/})
+    __command_successful || return
+    _wanted branch-names expl branch-name compadd $* - $branch_names
 }
